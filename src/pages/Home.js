cb76aab14d50bf7fd6704b46fb6549e790b2da73
JoaoMaFase <nascc.joao@gmail.com>
2023-08-25 16:24:05 -0300

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,21 +27,57 @@ function Home() {
         </div>
         <h1>ÁREA ADMINISTRATIVA</h1>
         {page == 1 &&
-          <section className='secao-conteudo'>
-            <h1>Controle de Clientes</h1>
-            <div>
-              <h1>Novo Cliente</h1>
-              <input type='text'/>
-              <input type='text'/>
-              <input type='text'/>
-              <input type='text'/>
-              <input type='text'/>
-              <a>Salvar</a>
-            </div>
-            <div>
+          <div className='conteudo-mae'>
+            <section className='secao-conteudo'>
+              <h1>Controle de Clientes</h1>
+              <div className='secao-forms-um'>
+                <h1>Novo Cliente</h1>
+                  <div className='input-box'>
+                    <input type='text' placeholder='Bruno'/>
+                    <label>Nome</label>
+                  </div>
+                  <div className='input-box'>
+                    <label>Email</label>
+                    <input type='text' placeholder='bruno@gmail.com'/>
+                  </div>
+                  <div className='input-box'>
+                    <label>Telefone</label>
+                    <input type='text' placeholder='(11) 99938-5764'/>
+                  </div>
+                  <div className='input-box'>
+                    <label>CPF</label>
+                    <input type='text' placeholder='323.323.232-33'/>
+                  </div>
+                  <div className='input-box'>
+                    <label>CNH</label>
+                    <input type='text' placeholder='06856723546897'/>
+                  </div>
+                <a>SALVAR</a>
+              </div>
+              <div>
 
-            </div>
-          </section>
+              </div>
+            </section>
+
+            <section className='secao-lista'>
+              <h1>Lista de Clientes</h1>
+              <div className='input-box'>
+                <label>Nome</label>
+                <input type='text' placeholder='Bruno'/>
+              </div>
+
+              <table>
+                <td>
+                  <th>Nome</th>           
+                  <th>Cpf</th> 
+                  <th>Cnh</th> 
+                  <th>Telefone</th> 
+                  <th>Email</th> 
+                </td>
+              </table>
+              
+            </section>
+          </div>
         }
         {}
         {}
