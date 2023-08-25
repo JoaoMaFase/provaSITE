1c05e13495275aef8acd6ab04fcad054f3d698fb
JoaoMaFase <nascc.joao@gmail.com>
2023-08-25 15:05:54 -0300

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './Home.scss';
 
 function Home() {
+  const [page,setPage] = useState(1)
   return (
     <div className="home">
       <section className='home-menu'>
@@ -10,10 +12,10 @@ function Home() {
         </div>
 
         <div className='menu-options'>
-          <a><img src='/home.svg'/>Home</a>
-          <a><img src='/user.svg'/>Clientes</a>
-          <a><img src='/car.png'/>Veículos</a>
-          <a><img src='/key.png'/>Locação</a>
+          <a onClick={() => setPage(1)} className={page == 1 ? 'hovered' : 'not-hovered'}><img src='/home.svg'/>Home</a>
+          <a onClick={() => setPage(2)} className={page == 2 ? 'hovered' : 'not-hovered'}><img src='/user.svg'/>Clientes</a>
+          <a onClick={() => setPage(3)} className={page == 3 ? 'hovered' : 'not-hovered'}><img src='/car.png'/>Veículos</a>
+          <a onClick={() => setPage(4)} className={page == 4 ? 'hovered' : 'not-hovered'}><img src='/key.png'/>Locação</a>
         </div>
       </section>
       <section className='home-conteudo'>
@@ -23,7 +25,24 @@ function Home() {
             <img src='/icon.png'/>
           </div>
         </div>
-        {}
+        <h1>ÁREA ADMINISTRATIVA</h1>
+        {page == 1 &&
+          <section className='secao-conteudo'>
+            <h1>Controle de Clientes</h1>
+            <div>
+              <h1>Novo Cliente</h1>
+              <input type='text'/>
+              <input type='text'/>
+              <input type='text'/>
+              <input type='text'/>
+              <input type='text'/>
+              <a>Salvar</a>
+            </div>
+            <div>
+
+            </div>
+          </section>
+        }
         {}
         {}
         {}
